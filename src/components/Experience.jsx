e5a94cb72832fd242eb3939f4230cf40b0b4a880
Parth <parthnakum20@gmail.com>
2025-03-01 16:04:40 +0530

    Initial setup

---
+++ src/components/Experience.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Reveal from './Reveal'
+
+
+
+
+
+const Experience = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Experience
